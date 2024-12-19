35ad282b2cd43e61d8ac862fec0c4058108765fc
satyam330 <satyamjha9132@gmail.com>
2024-12-19 12:48:19 +0530

    bubble sort program

--- JAVA/BubbleSort.java
+++ JAVA/BubbleSort.java
@@ -11,7 +11,6 @@ public class BubbleSort {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
-                    
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
@@ -25,6 +24,6 @@ public class BubbleSort {
         }
     }
     public static void main(String[] args) {
-        BubbleSort.bubbleSort(); // Call the bubbleSort method
+        BubbleSort.bubbleSort(); 
     }
 }
