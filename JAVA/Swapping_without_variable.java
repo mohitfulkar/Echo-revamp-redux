818369d5fe1ecc2d7dc0533b6e4ba60cebea8b21
satyam330 <satyamjha9132@gmail.com>
2024-12-22 14:21:21 +0530

    swap without third variable

--- JAVA/Swapping_without_variable.java
+++ JAVA/Swapping_without_variable.java
@@ -2,14 +2,16 @@ package JAVA ;
 
 public class Swapping_without_variable {
     public static void main(String[] args) {
-      // int temp ;
-       int a = 20 ;
-       int b =10 ;
-       System.out.println("Before swapping, a and b is :"+a + " and " + b);
-       a=a-b;
-       b=a+b;
-       a= b-a;
-       System.out.println("After swapping a and b is :"+a + " and " + b);
+      Swapping_without_variable.swapping();
+    }
+    public static void swapping(){
+      int a = 20 ;
+      int b =10 ;
+      System.out.println("Before swapping, a and b is :"+a + " and " + b);
+      a=a-b;
+      b=a+b;
+      a= b-a;
+      System.out.println("After swapping a and b is :"+a + " and " + b);
     }
     
 }
