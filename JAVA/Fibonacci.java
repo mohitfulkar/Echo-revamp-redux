048b3fbc993ad56308114aa0e225a0d0244cd2b0
satyam330 <satyamjha9132@gmail.com>
2024-12-25 22:59:01 +0530

    fibonacci program

--- JAVA/Fibonacci.java
+++ JAVA/Fibonacci.java
@@ -1,9 +1,14 @@
 package JAVA ;
+import java.util.Scanner;
 public class Fibonacci{
         public static void main(String[] args){
-         fibonacci(10);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            int n = sc.nextInt();
+         fibonacci(n);
         }
         public static void fibonacci(int n){
+
             int first =0, second = 1;
             System.out.println("Fibonacci Series up to "+n+" numbers");
             for(int i=1;i<=n;i++){
