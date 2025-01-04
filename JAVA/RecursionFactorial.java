58b7c5bcdc28bbce3c12677c2edc55a3e3efaa55
satyam330 <satyamjha9132@gmail.com>
2025-01-04 14:45:36 +0530

    factorial_recursion

--- JAVA/RecursionFactorial.java
+++ JAVA/RecursionFactorial.java
@@ -3,7 +3,6 @@ public class RecursionFactorial {
         int number = 5; 
         System.out.println("Factorial of " + number + " is: " + factorial(number));
     }
-
     public static int factorial(int n) {
         if (n <= 1) {
             return 1; 
