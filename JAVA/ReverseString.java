e18442e03531f40acb8bda3dac2dc297e31d3ff9
diksha-chaurasia2309 <dikshachaurasia2309@gmail.com>
2025-01-08 13:06:45 +0530

    creating file for reverseStringCode

---
+++ JAVA/ReverseString.java
@@ -0,0 +1,19 @@
+public class ReverseString {
+
+    public static void reverseString(String str, int n){
+        String rev = "";
+        for(int i=n-1; i>=0; i--){
+            rev += str.charAt(i);
+        }
+
+        System.out.println(rev);
+
+    }
+    public static void main(String[] args){
+        String str = "Diksha Chaurasia";
+        int n = str.length();
+        reverseString(str,n);
+
+
+    }
+}
